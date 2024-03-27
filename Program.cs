d160af5aed4347fc0a58be1e72d21ab1b4f91fe0
polidow <gabriel.polido01@etec.sp.gov.br>
2024-03-27 17:04:39 -0300

    lição feita

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Soletrar");
+
+Console.Write("escreva aqui seu texto: ");
+string carro;
+carro=Console.ReadLine()!;
+for (int i = 0; i < carro.Length; i++)
+{
+    Console.Write(carro[i]);
+    if (i < carro.Length-1)
+    {
+        Console.Write("-");
+    }
+}
+Console.WriteLine();
